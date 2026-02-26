051adbb47698a5412b6db08506b485ebdb40ce86
Leandro Forain <forain@gmail.com>
2026-02-26 17:31:37 -0300

    Migrate unifi-controller to unifi-network-application with external MongoDB
    - Replace linuxserver/unifi-controller with linuxserver/unifi-network-application
    - Add mongo:7.0 service with init script to create unifi DB user
    - Move MONGO_PASS and PIHOLE_WEBPASSWORD to .env file
    - Add .gitignore to exclude .env

---
+++ init-mongo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mongosh admin <<EOF
+db.getSiblingDB("unifi").createUser({
+  user: "unifi",
+  pwd: "${MONGO_PASS}",
+  roles: [{ role: "dbOwner", db: "unifi" }]
+});
+db.getSiblingDB("unifi_stat").createUser({
+  user: "unifi",
+  pwd: "${MONGO_PASS}",
+  roles: [{ role: "dbOwner", db: "unifi_stat" }]
+});
+EOF
